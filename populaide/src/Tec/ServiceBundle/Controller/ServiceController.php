d3d7ea785900bbee54d8e994bc260ce60d5715b3
Tracy <technobellaboratoire@outlook.fr>
2015-09-29 16:25:16 +0200

    Tracy update ServiceController; routing du ServiceBundle; results -> uniformisation avec le template de base

--- populaide/src/Tec/ServiceBundle/Controller/ServiceController.php
+++ populaide/src/Tec/ServiceBundle/Controller/ServiceController.php
@@ -3,16 +3,15 @@
 namespace Tec\ServiceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//
-//class DefaultController extends Controller
-//{
-//    public function indexAction()
-//    {
-//        return $this->render('TecServiceBundle:Default:index.html.twig');
-//    }
-//}
+
+
 class ServiceController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('TecServiceBundle:Default:index.html.twig');
+    }
+    
     public function resultsAction()
     {
         return $this->render('TecServiceBundle::results.html.twig');
